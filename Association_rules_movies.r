0db1cf5b6b8c69b96a3e5b9156782153c51c1004
preethi928 <67475727+preethi928@users.noreply.github.com>
2020-08-03 19:27:00 +0530

    Add files via upload
    For huge data there will be thousands of rules generated, this also leads to redundant rules.
    This code explains how to eliminate redundant rules in association rules.

--- Association_rules_movies.r
+++ Association_rules_movies.r
@@ -1,5 +1,5 @@
 library("arules")
-movies<-read.csv("C:\\Data_science\\EXCLER\\My Assignments\\my_movies.csv")
+movies<-read.csv("C:\\Data_science\\EXCLER\\My Assignments\\Association Rules\\my_movies.csv")
 str(movies)
 movies$Sixth.Sense = as.factor(movies$Sixth.Sense)
 movies$Gladiator = as.factor(movies$Gladiator)
@@ -12,8 +12,49 @@ movies$Patriot = as.factor(movies$Patriot)
 movies$Braveheart = as.factor(movies$Braveheart)
 movies$Green.Mile = as.factor(movies$Green.Mile)
 str(movies)
+#--------------------------Model 1-----------------------------------------------
 rules<-apriori(movies)
 arules::inspect(rules)
 rules2<-apriori(movies , parameter = list(minlen=1, maxlen=15, supp=0.2,conf=0.6),
                 appearance = list(lhs="V1=Sixth Sense"))
 arules::inspect(head(rules2, n =5,by = 'lift'))
+
+#------------------------Model 2-------------------------------------------------
+rules3<-apriori(movies)
+arules::inspect(head(n=100,rules3))
+sorted.rules3<-sort(rules3, by="lift")
+rules3<-apriori(movies , parameter = list(minlen=1, maxlen=15, supp=0.5,conf=0.6),
+                appearance = list(rhs="Braveheart=0"))
+arules::inspect(head(rules3, n =100,by = 'lift'))
+#---------------------same model with different support and confidence values --------
+
+rules3<-apriori(movies)
+arules::inspect(head(n=100,rules3))
+sorted.rules3<-sort(rules3, by="lift")
+rules3<-apriori(movies , parameter = list(minlen=1, maxlen=15, supp=0.7,conf=0.9),
+                appearance = list(rhs="Braveheart=0"))
+arules::inspect(head(rules3, n =100,by = 'lift'))
+
+# there is only one strong rule for conf=0.9
+#-------------------------changing support value-------------------
+
+rules3<-apriori(movies)
+arules::inspect(head(n=100,rules3))
+sorted.rules3<-sort(rules3, by="lift")
+rules3<-apriori(movies , parameter = list(minlen=1, maxlen=15, supp=0.5,conf=0.9),
+                appearance = list(rhs="Braveheart=0"))
+arules::inspect(head(rules3, n =100,by = 'lift'))
+
+#-----------------------Increasing confidence to 2.0----------------
+rules3<-apriori(movies)
+arules::inspect(head(n=100,rules3))
+sorted.rules3<-sort(rules3, by="lift")
+rules3<-apriori(movies , parameter = list(minlen=1, maxlen=15, supp=0.7,conf=0.9),
+                appearance = list(rhs="Braveheart=0"))
+arules::inspect(head(rules3, n =100,by = 'lift'))
+
+# maximum support and confidence values can be 0.9 and 0.9 
+
+
+
+
